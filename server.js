650113efa304979514c176fa251f1ead725f687f
April Larsen <hunterapril94@gmail.com>
2022-10-28 17:47:35 -0700

    plan post routes

--- server.js
+++ server.js
@@ -5,16 +5,33 @@ const PORT = process.env.PORT || 8080;
 const express = require("express");
 const app = express();
 const cookieParser = require('cookie-parser');
+const bodyparser = require('body-parser')
 // use middleware
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended: true}));
 // get routes
 app.get( "/", (req, res) => {
   res.render('index')
 })
 // post routes
+app.post("/newPlayer", (req, res) => {
+
+})
+app.post("/newGame", (req, res) => {
+
+})
+app.post("/deal", (req, res) => {
+
+})
+app.post("/playCard", (req, res) => {
+
+})
+app.post("/selectWinner", (req, res)=>{
+  
+})
 
 
 
